8481d4b0da9da5245569c263347d4c6b10356fe5
Karan <prajaktapawankar29@gmail.com>
2026-05-29 19:22:08 +0530

    fix post model naming

--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const Post = require("../models/Post");
+const Post = require("../models/post");
 
 router.post("/", async (req, res) => {
 
